06b5835bb7f6490817cbc4f7dd024661b33140a5
giannoskom <76660816+giannoskom@users.noreply.github.com>
2023-09-10 21:58:22 +0300

    Add files via upload

--- tables.sql
+++ tables.sql
@@ -15,6 +15,8 @@ INSERT INTO users (username, password, enabled) VALUES
 ('user1', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 't'),
 ('user2', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 't'),
 ('root', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 't'),
+('pr1', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 't'),
+('pr2', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 't'),
 ('admin', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 't');
 
 
@@ -23,7 +25,9 @@ INSERT INTO authorities(username, authority) VALUES
 ('admin', 'ROLE_ADMIN'),
 ('admin', 'ROLE_USER'),
 ('user1', 'ROLE_USER'),
-('user2', 'ROLE_USER');
+('user2', 'ROLE_USER'),
+('pr1', 'ROLE_PRODUCER'),
+('pr2', 'ROLE_PRODUCER');
 
 
 create table Rent (
@@ -35,3 +39,13 @@ create table Rent (
         renterName varchar(255),
         primary key (id)
     )
+    
+INSERT INTO car(username, authority) VALUES
+('toyota', 'yaris', '1999'),
+('bmw', '120i', '2007'),
+('volvo', 'xc40', '2020');
+
+insert into produce (carmodel, caryear, email, producername, brand) values 
+('3008', '2020', 'gkom@gmail.com', 'giannos komninos', 'peugeut'),
+('Q5', '2015', 'gfov@gmail.com', 'giannis fovakis', 'audi');
+
